ffb7e9c54b6d7a56ab6687fb6164983197f2f744
Adexux96 <100967703+Adexux96@users.noreply.github.com>
2024-07-15 00:06:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleMode = document.getElementById('toggle-mode');
-    const body = document.body;
+const toggleModeCheckbox = document.getElementById('toggle-mode');
 
-    // Cambia la clase del cuerpo al hacer clic en el interruptor
-    toggleMode.addEventListener('change', function () {
-        body.classList.toggle('light-mode');
-        body.classList.toggle('dark-mode');
-
-        // Actualiza la etiqueta del interruptor
-        const toggleLabel = document.querySelector('.toggle-label');
-        toggleLabel.textContent = body.classList.contains('dark-mode') ? 'Modo Oscuro' : 'Modo Claro';
-    });
+toggleModeCheckbox.addEventListener('change', function() {
+    document.body.classList.toggle('light-mode');
+    document.body.classList.toggle('dark-mode');
 });
