750e291bd9f61943461420433291a7b1907d475a
Adexux96 <100967703+Adexux96@users.noreply.github.com>
2024-07-15 01:20:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const toggleMode = document.querySelector('.toggle-mode');
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleMode = document.querySelector('.toggle-mode');
+    const toggleLabel = document.querySelector('.toggle-label');
 
-toggleMode.addEventListener('change', function() {
-    document.body.classList.toggle('light-mode');
+    toggleMode.addEventListener('change', function() {
+        if (toggleMode.checked) {
+            document.body.classList.add('light-mode');
+            toggleLabel.textContent = 'Modo Claro'; // Cambia el texto del label al modo claro
+        } else {
+            document.body.classList.remove('light-mode');
+            toggleLabel.textContent = 'Modo Oscuro'; // Vuelve a cambiar el texto del label al modo oscuro
+        }
+    });
 });
